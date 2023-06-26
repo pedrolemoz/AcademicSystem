c2017fee5442857e1635830d5a487a8289ec0c99
Pedro Lemos <lemozxpedro@gmail.com>
2023-06-25 23:15:11 -0300

    WIP

--- src/main/java/com/pedrolemoz/academic/AcademicSystemApplication.java
+++ src/main/java/com/pedrolemoz/academic/AcademicSystemApplication.java
@@ -13,7 +13,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 
 @SpringBootApplication
 public class AcademicSystemApplication implements CommandLineRunner {
